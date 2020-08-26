bd7bc101e0388435b47b095d433262dea671bff8
dbogdanov <dmitry.bogdanov.es@gmail.com>
2020-08-27 00:13:47 +0300

    start Traing procedure

--- classification/logreg.go
+++ classification/logreg.go
@@ -3,11 +3,10 @@ package logreg
 import (
 	"fmt"
 	"math"
+	"math/rand"
 )
 
 type LogisticRegression struct {
-	trainX        [][]float64
-	trainY        []float64
 	weights       []float64
 	fit_intercept bool
 }
@@ -16,8 +15,54 @@ func New(fit_intercept bool) *LogisticRegression {
 	return &LogisticRegression{fit_intercept: fit_intercept}
 }
 
-func (lr LogisticRegression) Train(trainX, trainY float64) error {
-	return fmt.Errorf("Not implemented")
+func (lr LogisticRegression) Train(trainX [][]float64, trainY []float64) error {
+	xLength := len(trainX)
+	yLength := len(trainY)
+	weightsLength := len(trainX[0])
+
+	if xLength != yLength {
+		return fmt.Errorf("Logreg train error: X and Y length missmatch: %v, %v", xLength, yLength)
+	}
+
+	inititalWeights := make([]float64, weightsLength)
+	for i, _ := range inititalWeights {
+		inititalWeights[i] = rand.Float64()
+	}
+
+	learningRate := 0.1
+	epsilon := 0.001
+	initialEpsilon := 9999.0
+
+	for initialEpsilon > epsilon {
+		totalGradient := make([]float64, weightsLength)
+
+		for i, object := range trainX {
+			partialGradient, err := logLossGradient(object, inititalWeights, trainY[i])
+			if err != nil {
+				return fmt.Errorf("Logreg train error: %v", err)
+			}
+
+			for i := range totalGradient {
+				totalGradient[i] += partialGradient[i]
+			}
+		}
+
+		for i := range totalGradient {
+			totalGradient[i] *= learningRate
+		}
+
+		for i := range inititalWeights {
+			inititalWeights[i] -= totalGradient[i]
+		}
+
+		initialEpsilon = totalGradient[0]
+		for _, component := range totalGradient {
+			initialEpsilon = math.Max(component, initialEpsilon)
+		}
+
+	}
+
+	return nil
 }
 
 func (lr LogisticRegression) PredictProbabilities(objects [][]float64) ([]float64, error) {
@@ -49,6 +94,21 @@ func sigmoid(x float64) float64 {
 	return 1.0 / (1 + math.Exp(-x))
 }
 
+func logLossGradient(object []float64, weights []float64, trueAnswer float64) ([]float64, error) {
+	result := make([]float64, len(weights))
+
+	multiplied, err := scalarMultiplication(weights, object)
+	if err != nil {
+		return nil, fmt.Errorf("logLossGradient error: %v", err)
+	}
+
+	for _, w := range weights {
+		result = append(result, w*(sigmoid(multiplied)-trueAnswer))
+	}
+
+	return result, nil
+}
+
 func logLoss(weights []float64, objects [][]float64, trueAnswers []float64) (float64, error) {
 	var result float64
 
@@ -62,7 +122,7 @@ func logLoss(weights []float64, objects [][]float64, trueAnswers []float64) (flo
 		result += -1*trueAnswers[i]*math.Log(predicted) - (1.0 - predicted)
 	}
 
-	return result, nil
+	return result / float64(len(objects)), nil
 }
 
 func MeanSquareError(weights []float64, objects [][]float64, trueAnswers []float64) (float64, error) {
