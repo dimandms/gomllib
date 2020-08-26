77c63ba79d4db22ff005d6feaffd95e9c1b2c5b4
dbogdanov <dmitry.bogdanov.es@gmail.com>
2020-08-26 22:25:03 +0300

    implement prediction functions

--- classification/logreg.go
+++ classification/logreg.go
@@ -8,6 +8,7 @@ import (
 type LogisticRegression struct {
 	trainX        [][]float64
 	trainY        []float64
+	weights       []float64
 	fit_intercept bool
 }
 
@@ -15,8 +16,53 @@ func New(fit_intercept bool) *LogisticRegression {
 	return &LogisticRegression{fit_intercept: fit_intercept}
 }
 
-func (lr LogisticRegression) Fit(trainX, trainY float64) error {
+func (lr LogisticRegression) Train(trainX, trainY float64) error {
 	return fmt.Errorf("Not implemented")
+}
+
+func (lr LogisticRegression) PredictProbabilities(objects [][]float64) ([]float64, error) {
+	result := make([]float64, 0)
+
+	for _, object := range objects {
+		p, err := lr.predictProbability(object)
+		if err != nil {
+			return nil, fmt.Errorf("Prediction error: %v", err)
+		}
+
+		result = append(result, p)
+	}
+
+	return result, nil
+}
+
+func (lr LogisticRegression) predictProbability(object []float64) (float64, error) {
+
+	multiplied, err := scalarMultiplication(lr.weights, object)
+	if err != nil {
+		return 0.0, fmt.Errorf("Prediction error: %v", err)
+	}
+
+	return sigmoid(multiplied), nil
+}
+
+func sigmoid(x float64) float64 {
+	return 1.0 / (1 + math.Exp(-x))
+}
+
+func logLoss(weights []float64, objects [][]float64, trueAnswers []float64) (float64, error) {
+	var result float64
+
+	for i, obj := range objects {
+		multiplied, err := scalarMultiplication(weights, obj)
+		if err != nil {
+			return 0.0, fmt.Errorf("logLoss calcualtion error: %v", err)
+		}
+		predicted := sigmoid(multiplied)
+
+		result += -1*trueAnswers[i]*math.Log(predicted) - (1.0 - predicted)
+	}
+
+	return result, nil
 }
 
 func MeanSquareError(weights []float64, objects [][]float64, trueAnswers []float64) (float64, error) {
